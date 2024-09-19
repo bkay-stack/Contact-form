503c9a25a46bf4a643e9ea42350ebf164d27cb7d
bkay-stack <kevinbest1900@gmail.com>
2024-09-19 12:56:45 +0100

    msg

--- src/component/ContactForm.jsx
+++ src/component/ContactForm.jsx
@@ -11,32 +11,62 @@ const ContactForm = () => {
       <h1>Contact form</h1>
       <div className="name-values">
         <div className="first">
-          <label htmlFor="your name">First Name:</label>
-          <input type="text" name="" id="" />
+          <label htmlFor="your name" className="label-styling">
+            First Name:
+          </label>
+          <input type="text" name="" className="input-styling" />
         </div>
         <div className="last">
-          <label htmlFor="your name">Last Name:</label>
-          <input type="text" name="" id="" />
+          <label htmlFor="your name" className="label-styling">
+            Last Name:
+          </label>
+          <input type="text" name="" id="" className="input-styling" />
         </div>
       </div>
       <div className="email">
-        <label htmlFor="email">Email:</label>
-        <input type="email" name="" id="" />
+        <label htmlFor="email" className="label-styling">
+          Email:
+        </label>
+        <input type="email" name="" id="" className="input-styling" />
       </div>
 
-      <label htmlFor="query">Query Type:</label>
+      <label htmlFor="query" className="label-styling">
+        Query Type:
+      </label>
       <div className="radios">
         {/* <div className="radio-wrap-1"> */}
-        <input type="text" />
+        <input type="text" className="input-styling" />
         {/* </div> */}
 
         {/* <div className="radio-wrap-2"> */}
-        <input type="text" />
+        <input type="text" className="input-styling" />
         {/* </div> */}
       </div>
       <div className="msg">
-        <label htmlFor="message">Message:</label>
-        <input type="text" />
+        <label htmlFor="message" className="label-styling">
+          Message:
+        </label>
+        <input type="text" className="input-styling" />
+      </div>
+      <div className="check-box">
+        <div className="check">
+          <input
+            type="checkbox"
+            id="custom-checkbox"
+            className="input-styling"
+          />
+        </div>
+        <p> I consent to being contacted by the team</p>
+      </div>
+
+      <div className="btn">
+        <button type="submit" className="">
+          Submit
+        </button>
+      </div>
+      <div>
+        <input type="radio" />
+        <label>Option text</label>
       </div>
     </form>
     // </div>
