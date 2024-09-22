45f21d4d6597c7d51256f67f1653be6e910593de
bkay-stack <kevinbest1900@gmail.com>
2024-09-22 16:41:17 +0100

    Card

--- src/component/ContactForm.jsx
+++ src/component/ContactForm.jsx
@@ -1,78 +1,92 @@
 import React from "react";
+import { useState } from "react";
 import "./contact.styles.css";
 import iconImg from "../assets/images/icon-radio-selected.svg";
 import iconCheck from "../assets/images/icon-checkbox-check.svg";
 import iconSuccess from "../assets/images/icon-success-check.svg";
 const ContactForm = () => {
+  const handleSubmit = () => {
+    alert("move");
+  };
+
   return (
-    // <div className="contact-form">
-
-    <form>
-      <h1>Contact form</h1>
-      <div className="name-values">
-        <div className="first">
-          <label htmlFor="your name" className="label-styling">
-            First Name:
+    <div className="contact-form">
+      <div className="pop-up">
+        <div className="content-wrap">
+          <div className="sent">
+            <img src={iconSuccess} alt="" />
+            <span>Message Sent!</span>
+          </div>
+          <p> Thanks for completing the form. We'll be in touch soon!</p>
+        </div>
+      </div>
+      <form>
+        <h1>Contact form</h1>
+        <div className="name-values">
+          <div className="first">
+            <label htmlFor="your name" className="label-styling">
+              First Name:
+            </label>
+            <input type="text" name="" className="input-styling" />
+          </div>
+          <div className="last">
+            <label htmlFor="your name" className="label-styling">
+              Last Name:
+            </label>
+            <input type="text" name="" id="" className="input-styling" />
+          </div>
+        </div>
+        <div className="email">
+          <label htmlFor="email" className="label-styling">
+            Email:
           </label>
-          <input type="text" name="" className="input-styling" />
+          <input type="email" name="" id="" className="input-styling" />
         </div>
-        <div className="last">
-          <label htmlFor="your name" className="label-styling">
-            Last Name:
+
+        <label htmlFor="query" className="label-styling">
+          Query Type:
+        </label>
+        <div className="radios">
+          <div className="radio1">
+            {/* <> */}{" "}
+            <input type="radio" id="huey" name="drone" value="huey" checked />
+            <label for="huey">Huey</label>
+            {/* </> */}
+          </div>
+          <div className="radio1">
+            <input type="radio" id="huey" name="drone" value="huey" checked />
+            <label for="huey">Huey</label>
+          </div>
+        </div>
+        <div className="msg">
+          <label htmlFor="message" className="label-styling">
+            Message:
           </label>
-          <input type="text" name="" id="" className="input-styling" />
+          <textarea
+            classNamename="msg"
+            name="message"
+            cols="30"
+            rows="10"
+          ></textarea>
         </div>
-      </div>
-      <div className="email">
-        <label htmlFor="email" className="label-styling">
-          Email:
-        </label>
-        <input type="email" name="" id="" className="input-styling" />
-      </div>
+        <div className="check-box">
+          <div className="check">
+            <input
+              type="checkbox"
+              id="custom-checkbox"
+              className="input-styling"
+            />
+          </div>
+          <p> I consent to being contacted by the team</p>
+        </div>
 
-      <label htmlFor="query" className="label-styling">
-        Query Type:
-      </label>
-      <div className="radios">
-        <div className="radio1">
-          {/* <> */}{" "}
-          <input type="radio" id="huey" name="drone" value="huey" checked />
-          <label for="huey">Huey</label>
-          {/* </> */}
+        <div className="btn">
+          <button type="submit" onClick={handleSubmit}>
+            Submit
+          </button>
         </div>
-        <div className="radio1">
-          <input type="radio" id="huey" name="drone" value="huey" checked />
-          <label for="huey">Huey</label>
-        </div>
-      </div>
-      <div className="msg">
-        <label htmlFor="message" className="label-styling">
-          Message:
-        </label>
-        <input type="text" className="input-styling" />
-      </div>
-      <div className="check-box">
-        <div className="check">
-          <input
-            type="checkbox"
-            id="custom-checkbox"
-            className="input-styling"
-          />
-        </div>
-        <p> I consent to being contacted by the team</p>
-      </div>
-
-      <div className="btn">
-        <button type="submit" className="">
-          Submit
-        </button>
-      </div>
-      <div>
-        <input type="radio" />
-        <label>Option text</label>
-      </div>
-    </form>
-    // </div>
+      </form>{" "}
+    </div>
   );
 };
 
